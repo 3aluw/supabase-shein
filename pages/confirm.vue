fdf0a062e1466f6e6f1ef2d98620a3af49de3107
Abdou <2000abdallah2014@gmail.com>
2025-09-03 13:49:26 +0100

    handle user sign in in confirm page

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -3,8 +3,22 @@ import type { Database } from '~/types/supabase'
 const user = useSupabaseUser()
 const client = useSupabaseClient<Database>()
 
+const checkIfExistingUser = async(role:"consumer" | "seller",userId:string) =>{
+  let isFound = null
+  if(role == "consumer"){
+    const consumerProfile =await  client.from("consumers").select("user_id").eq("user_id",userId).maybeSingle()
+    console.log('consumerProfile: ', consumerProfile);
+    isFound = consumerProfile.data
+  }else if(role="seller"){
+    const consumerProfile = await client.from("sellers").select("user_id").eq("user_id",userId).maybeSingle()
+    console.log('consumerProfile: ', consumerProfile);
+    isFound = consumerProfile.data
+  }
+  return isFound == null ? false : true
+}
+
 onMounted(() => {
-  console.log("mounted");
+  console.log("on confirm");
   handleUser()
 })
 import { jwtDecode } from 'jwt-decode';
@@ -18,10 +32,28 @@ const handleUser = async () => {
       const decodedToken = jwtDecode(accessToken);
       // The custom claims are directly on the decoded payload
      const  userRole = decodedToken.user_role;
+    const isExisting =await checkIfExistingUser(userRole,session.user.id)
+    const toRoute = isExisting ? "/" : userRole === "consumer" ? "/cn" : "/sl" 
+    console.log(isExisting,userRole,toRoute);
+    return navigateTo(toRoute)
   }
 }
 </script>
 
 <template>
-  <div>Waiting for login...</div>
+ <template>
+  <div class="flex items-center justify-center h-screen bg-gray-50">
+    <div class="flex flex-col items-center space-y-4">
+      <!-- Vuetify loader -->
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        size="48"
+      ></v-progress-circular>
+
+      <!-- Text -->
+      <p class="text-lg font-medium text-gray-700">يتم تسجيل دخولك</p>
+    </div>
+  </div>
+</template>
 </template>
